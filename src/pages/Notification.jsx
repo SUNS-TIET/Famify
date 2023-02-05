c9122dcf5d31e5fc95783bcdfb623337bbaa1eb1
Sahir Khan <81610501+SkaiLord@users.noreply.github.com>
2023-02-05 11:22:54 +0530

    Notification

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -9,8 +9,8 @@ export default function Notification() {
           bg="#df6005"
           color="white"
           onClick={() => toast({
-            title: 'Bits',
-            description: "Today's Bit: SIP is a way of putting a small amount of money into a special savings plan (like a mutual fund) every month instead of one big lump sum. This helps you save up over time and also helps you not worry so much about the ups and downs of the market.",
+            title: 'Today's Bit',
+            description: "SIP is a way of putting a small amount of money into a special savings plan (like a mutual fund) every month instead of one big lump sum. This helps you save up over time and also helps you not worry so much about the ups and downs of the market.",
             duration: 10000,
             isClosable: true,
             position: 'top',
